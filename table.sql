d0c1c2cde9d5d6a1842dbd943ad0523e627bfc53
semteacher <semteacher@gmail.com>
2015-02-15 14:45:15 +0200

    Pregnacy CDSS Form to the OpenEMR based on the form example able to save/load data 0.1 version

---
+++ table.sql
@@ -0,0 +1,31 @@
+/* 
+ * create a custom table for the form
+ *
+ * This table NEEDS a UNIQUE name
+ */
+
+CREATE TABLE IF NOT EXISTS `pregnacy_cdss_patient_data` (
+    /* these fields are common to all forms and should remain intact */
+    id bigint(20) NOT NULL auto_increment,
+    date datetime default NULL,
+    pid bigint(20) default NULL,
+    user varchar(255) default NULL,
+    groupname varchar(255) default NULL,
+    authorized tinyint(4) default NULL,
+    activity tinyint(4) default NULL,
+
+    /* these fields are customized to this form */
+    form_date       datetime default NULL,  /* date the form was completed by client */
+    name            varchar(255),           /* full name on the form */
+    dob             datetime default NULL,  /* date of birth */
+    phone           varchar(15),            /* phone number */
+    address         varchar(255),           /* home address */
+    notes           longtext,               /* free-text notes */
+    sig             char(1),                /* Did client sign the paper version of the form? */
+    sig_date        datetime default NULL,  /* Date the client signed the form */
+    qst11_age       tinyint(4) default NULL, /*1.1. Вік жінки*/
+    qst12_season    tinyint(4) default NULL, /*1.2. Сезон року*/
+    /* end of custom form fields */
+
+    PRIMARY KEY (id) 
+) ENGINE=MyISAM;
